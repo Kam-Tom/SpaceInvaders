c3655f71b363d15914b949215105b3621a34599c
Kam-Tom <kamil8tomaszewski@gmail.com>
2024-01-11 20:47:13 +0100

    Added drawable abstact class -ABC: throws error when creting Drawable -@abstractmethod: throws error when child class dont implement this method

---
+++ Drawable.py
@@ -0,0 +1,12 @@
+import pygame
+from abc import ABC, abstractmethod 
+
+class Drawable(pygame.sprite.Sprite,ABC):
+    
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def draw(self, surface):
+        pass
