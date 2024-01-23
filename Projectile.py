53d16533bfcf0b5dd933a1890fe795651a8d6417
xjkxksj <106696465+xjkxksj@users.noreply.github.com>
2024-01-23 20:58:15 +0100

    Bye Decorator + more comments

--- Projectile.py
+++ Projectile.py
@@ -7,6 +7,7 @@ class Projectile(Drawable):
         self.on_disable = on_disable
         self.size = (16,20)
 
+    # Enable the projectile with given x, y coordinates
     def enable(self, x, y):
         self.pos = (x,y)
         self.rect.center = (x, y)
@@ -14,10 +15,10 @@ class Projectile(Drawable):
     def set_type(self,tag:str,velocity:float):
         self.velocity = velocity
         self.tag = tag
+        # Load and scale the image of the projectile
         self.image = pygame.image.load("Sprites/"+tag+".png")
         self.image = pygame.transform.scale(self.image, self.size)
         self.rect = self.image.get_rect()
-    
 
     def disable(self):
         self.on_disable(self)
@@ -25,15 +26,16 @@ class Projectile(Drawable):
     def draw(self, surface):
         surface.blit(self.image, self.pos)
 
+    # Update the position of the projectile
     def update(self):
         self.pos = (self.pos[0], self.pos[1] - self.velocity)
         self.rect.center = self.pos
         if self.over_screen():
             self.disable()
 
+    # Check if the projectile is over the screen
     def over_screen(self):
         return not (self.pos[1] > 0 and self.pos[1] < SCREEN_HEIGHT)
     
     def check_colision(self, obj):
         pass
-    
